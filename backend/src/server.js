9b3c1a7ba876447f6f0560b23348770239ca0788
Aniveshsingh <aniveshsingh1242@gmail.com>
2026-04-13 11:27:59 +0530

    cors updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,6 +19,7 @@ app.use(
     origin: [
       "https://ai-recipe-generator-ltx9qu7m7-aniveshsingh1242-3035s-projects.vercel.app",
       "https://ai-recipe-generator-mu-rouge.vercel.app",
+      "https://ai-mealmatrix.vercel.app",
     ],
     credentials: true,
   }),
